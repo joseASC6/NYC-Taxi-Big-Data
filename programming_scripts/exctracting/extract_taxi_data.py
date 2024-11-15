390b12d2aeeaaeed5f05c8b0fc552d46a32364e4
joseASC6 <jose.mauricio0226@gmail.com>
2024-11-15 01:26:56 -0500

    Started feature engineering
    Updated programming file layout
    Bugs in cleaning data scripts fixed
    Began working with Py Spark and feature engineering
    Model was tested but not working

---
+++ programming_scripts/exctracting/extract_taxi_data.py
@@ -0,0 +1,46 @@
+# Imports
+from google.cloud import storage
+import requests
+
+
+# Create a client object that points to GCS
+storage_client = storage.Client()
+
+# Define the bucket
+bucket_name = 'my-bigdataproject-jg'
+bucket = storage_client.get_bucket(bucket_name)
+
+# Destination folder
+destination_folder = 'landing/'
+
+# Taxi Data URL information
+taxi_data_base_url = "https://d37ci6vzurychx.cloudfront.net/trip-data/"
+data_years = [2021, 2022, 2023]
+months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+
+Headers = {'user-agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
+
+session = requests.Session()
+
+
+def extract_taxi_data(file_name):
+    print(f"Extracting file: \t{file_name}")
+    full_url = taxi_data_base_url + file_name
+    response = session.get(full_url, headers=Headers)
+    print(f"Found file: \t{file_name}")
+    data = response.content
+    print(f"Uploading to GCS: \t{file_name}")
+    blob = bucket.blob(destination_folder + file_name)
+    blob.upload_from_string(data)
+    print(f"Upload complete: \t{file_name}")
+
+if __name__ == "__main__":
+    # Loop through the taxi data ids keys
+    print("Downloading taxi data...")
+    for year in data_years:
+        for month in months:
+            file_name = f"yellow_tripdata_{year}-{month}.parquet"
+            extract_taxi_data(file_name)
+    print("Download complete.")
+            
+
---
+++ programming_scripts/exctracting/extract_weather_data.py
@@ -0,0 +1,67 @@
+from google.cloud import storage
+import pandas as pd
+from io import StringIO
+from datetime import datetime
+import requests
+
+# Create a client object that points to GCS
+storage_client = storage.Client()
+
+# Define the bucket
+bucket_name = 'my-bigdataproject-jg'
+bucket = storage_client.get_bucket(bucket_name)
+
+# Destination folder
+destination_folder = 'landing/'
+
+# Weather API Request URL example:
+# https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY 
+
+base_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+
+boroughs = ['Manhattan', 'Brooklyn', 'Queens', 'Bronx', 'Staten Island']
+years = [2021, 2022, 2023]
+key = "5ZWZZVN2Q99L955WE2T42Y38B" # Replace with your own API key
+params = {
+    "key" : key,
+    "contentType" : "csv",
+    "include" : "days"
+}
+
+def extract_weather_data(borough, year):
+    start_date = f"{year}-01-01"
+    end_date = f"{year}-12-31"
+    location = f"{borough}, NY"
+
+    url = f"{base_url}{location}/{start_date}/{end_date}"
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        df = pd.read_csv(StringIO(response.text))
+        file_name = f"weather_data_{borough}_{year}.csv"
+        print(f"Saving to CSV: \t{file_name}")
+        csv_buffer = df.to_csv(index=False)
+        blob = bucket.blob(destination_folder + file_name)
+        blob.upload_from_string(csv_buffer, content_type='text/csv')
+        print(f"Upload complete: \t{file_name}")
+    else:
+        print(f"Error: {response.status_code}")
+
+
+if __name__ == "__main__":
+    print("Choose a borough:")
+    for i, borough in enumerate(boroughs):
+        print(f"{i+1}. {borough}")
+    borough = int(input("Select a borough:\t")) - 1
+
+    print("Choose a year:")
+    for i, year in enumerate(years):
+        print(f"{i+1}. {year}")
+    year = int(input("Select the year:\t")) - 1
+
+    borough = boroughs[borough]
+    year = years[year]
+
+    print(f"Extracting weather data for {borough} in {year}")
+
+    extract_weather_data(borough, year)
